440a566c5d9b2067e403ffb4744fc8eb4e59e03a
Jmingus <jmingus@austin.rr.com>
2015-09-03 17:04:35 -0500

    Wrote, playerturn logic, with valid input formatter using regex.

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var clear = require('cli-clear');
 var prompt = require('prompt-sync').prompt;
-
+var turnCounter = 0;
+var regex = /[1-3]\s[1-3]/;
 var player1 = {};
 var player2 = {};
-var player1Move = null;
-var player2Move = null;
+var winFlag = false;
+var currentPlayer = null;
+var cleanMove = null;
+
 var board = [
     [' ',' ',' '],
     [' ',' ',' '],
@@ -23,6 +26,23 @@ var printBoard = function(){
         '   ~~~~~~~~~~~~~\n')
 };
 
+var validInput = function(regex, move){
+    if(regex.test(move)){
+       cleanMove = currentPlayer.move.split(" ")
+    }else{
+        console.log("Try Again, Numbers must between 1 and 3" +
+            " plus contain a space in between them");
+        currentPlayer.move = prompt();
+    };
+}
+
+var playerTurn = function(turnCounter){
+    if (turnCounter % 2 === 0) {
+        currentPlayer = player1;
+    }else{
+        currentPlayer = player2;
+    }
+};
 console.log('Welcome to the game of Tic-Tac-Toe!');
 console.log("-----------------------------------");
 console.log('Player 1 what is your name?');
@@ -31,3 +51,14 @@ console.log('Player 2 what is your name?');
 player2.name = prompt();
 
 printBoard();
+clear();
+
+while(winFlag = false || turnCounter < 9){
+    playerTurn(turnCounter);
+    turnCounter++;
+    console.log(currentPlayer.name + ' it is your turn!');
+    console.log('Enter your move now!');
+    validInput(regex, currentPlayer.move = prompt());
+
+
+}
