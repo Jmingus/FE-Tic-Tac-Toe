f428dd0edfa720439d1b7da30991bfd8ebc7428e
Jmingus <jmingus@austin.rr.com>
2015-09-06 21:59:46 -0500

    Finished forfeit and replay works, but needs to be worked on

--- main.js
+++ main.js
@@ -72,42 +72,74 @@ var checkIfWon = function(board, winningCombo){
     }
 };
 
-console.log('Welcome to the game of Tic-Tac-Toe!'.rainbow);
-console.log("-----------------------------------");
-console.log('Player 1 what is your name?'); player1.name = prompt();
-console.log('Your X\n'.red);
-console.log("-----------------------------------");
-console.log('Player 2 what is your name?'); player2.name = prompt();
-console.log('Your O\n'.blue);
-console.log("-----------------------------------\n");
-printBoard();
-
-while((winFlag === false) || (turnCounter < 9)){
-    playerTurn(turnCounter);
-    turnCounter++;
-    console.log(currentPlayer.name + ' it is your turn!');
-    console.log('Enter your move now!'.magenta);
-    currentPlayer.move = prompt();
-    while(formattedMove === null) {
-        validInput(regex, currentPlayer.move);
-        var xCoord = formattedMove[0];
-        var yCoord = formattedMove[1];
-        if (board[yCoord - 1][xCoord - 1] === player1.mark || board[yCoord - 1][xCoord - 1] === player2.mark){
-            console.log("That move is already taken!!");
-            formattedMove = null;
-            currentPlayer.move = prompt();
-        }else {
-            board[yCoord - 1][xCoord - 1] = currentPlayer.mark;
-        }
-    };
-    printBoard();
-    var winningCombo = currentPlayer.mark + currentPlayer.mark + currentPlayer.mark;
-    if (checkIfWon(board, winningCombo) === true){
-        console.log(currentPlayer.name + ' ' + 'has won!');
-        winFlag = true;
-        break;
+var replayGame = function(){
+    console.log('Would you like to play again? (yes/no)');
+    answer = prompt();
+    if (answer.toLowerCase() === 'yes'){
+        turnCounter = 0;
+        board = [
+            [' ',' ',' '],
+            [' ',' ',' '],
+            [' ',' ',' ']
+        ];
+        var winFlag = false;
+        game();
+        clear();
+    }else {
+        clear();
     }
-    console.log(checkIfWon(board, winningCombo));
-    console.log(winFlag)
-    formattedMove = null;
-}
+};
+var game = function() {
+    console.log('Welcome to the game of Tic-Tac-Toe!'.rainbow);
+    console.log("-----------------------------------");
+    console.log('Player 1 what is your name?');
+    player1.name = prompt();
+    console.log('Your X\n'.red);
+    console.log("-----------------------------------");
+    console.log('Player 2 what is your name?');
+    player2.name = prompt();
+    console.log('Your O\n'.blue);
+    console.log("-----------------------------------\n");
+    printBoard();
+
+    while ((winFlag === false) || (turnCounter < 9)) {
+        playerTurn(turnCounter);
+        turnCounter++;
+        console.log(currentPlayer.name + ' it is your turn!');
+        console.log('Enter your move now!'.magenta);
+        currentPlayer.move = prompt();
+        if (currentPlayer.move.toLowerCase() === 'forfeit') {
+            if (currentPlayer === player1) {
+                console.log(player2.name + " has won!");
+                break;
+            } else {
+                console.log(player1.name + " has won!");
+                break;
+            }
+        }
+        while (formattedMove === null) {
+            validInput(regex, currentPlayer.move);
+            var xCoord = formattedMove[0];
+            var yCoord = formattedMove[1];
+            if (board[yCoord - 1][xCoord - 1] === player1.mark || board[yCoord - 1][xCoord - 1] === player2.mark) {
+                console.log("That move is already taken!!");
+                formattedMove = null;
+                currentPlayer.move = prompt();
+            } else {
+                board[yCoord - 1][xCoord - 1] = currentPlayer.mark;
+            }
+        }
+        ;
+        printBoard();
+        var winningCombo = currentPlayer.mark + currentPlayer.mark + currentPlayer.mark;
+        if (checkIfWon(board, winningCombo) === true) {
+            console.log(currentPlayer.name + ' ' + 'has won!');
+            winFlag = true;
+            break;
+        }
+        formattedMove = null;
+    };
+    replayGame();
+};
+
+game();
